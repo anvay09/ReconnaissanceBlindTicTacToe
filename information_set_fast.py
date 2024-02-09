615eb813d515361d989ce086d8de12378b364cb7
anvay09 <shah.anvay@gmail.com>
2024-02-09 19:13:33 +0530

    Trying multiprocessing for P1 action

--- information_set_fast.py
+++ information_set_fast.py
@@ -214,27 +214,59 @@ def play(I_1, I_2, true_board, player, move_flag=True):
 
 
 if __name__ == "__main__":
-    true_board = TicTacToeBoard(board=['o','0','0','0','x','0','x','0','0'])
-    I_1 = InformationSet(player='x', board=['o','-','-','-','x','-','x','-','-'])
+    true_board = TicTacToeBoard(board=['o','0','0','0','x','0','0','0','0'])
+    I_1 = InformationSet(player='x', board=['o','0','-','0','x','-','-','-','-'])
     I_2 = InformationSet(player='o', board=['o','-','-','-','x','-','-','-','-'])
-    player = 'o'
+    player = 'x'
     move_flag = True
+
+    I_1_vars = []
+    I_2_vars = []
+    true_board_vars = []
+
+    states = I_1.get_states()
+    actions = I_1.get_actions(move_flag)
+    
+    for action in actions:
+        output_states = []
+        for state in states:
+            new_state = state.copy()
+            valid = new_state.update_move(action, player)
+
+            if new_state.is_over() or new_state.is_win()[0] or not valid:
+                pass
+            else:
+                output_states.append(new_state)
+
+        new_true_board = true_board.copy()
+        success = new_true_board.update_move(action, player)
+        
+        if success and not new_true_board.is_win()[0] and not new_true_board.is_over():    
+            new_I = get_information_set_from_states(output_states, player)
+            new_I.reset_zeros()
+
+            I_1_vars.append(new_I)
+            I_2_vars.append(I_2.copy())
+            true_board_vars.append(new_true_board)
+    
+    with Pool(len(I_1_vars)) as pool:
+        obj_list = pool.starmap(play, [(I_1_vars[i], I_2_vars[i], true_board_vars[i], 'o', 'False') for i in range(len(I_1_vars))])
 
     # num_H = play(I_1, I_2, true_board, player, move_flag)
     # print("Histories: ", num_H)
 
-    I_1_vars = [I_1.copy(), I_1.copy(), I_1.copy(), I_1.copy()]
-    I_2_vars = [I_2.copy(), I_2.copy(), I_2.copy(), I_2.copy()]
-    true_board_vars = [true_board.copy(), true_board.copy(), true_board.copy(), true_board.copy()]
+    # I_1_vars = [I_1.copy(), I_1.copy(), I_1.copy(), I_1.copy()]
+    # I_2_vars = [I_2.copy(), I_2.copy(), I_2.copy(), I_2.copy()]
+    # true_board_vars = [true_board.copy(), true_board.copy(), true_board.copy(), true_board.copy()]
 
-    keys = list(I_2.sense_square_dict.keys())
-    for i in range(4):
-        I_2_vars[i].simulate_sense(keys[i], true_board)  
+    # keys = list(I_2.sense_square_dict.keys())
+    # for i in range(4):
+    #     I_2_vars[i].simulate_sense(keys[i], true_board)  
 
-    with Pool(4) as pool:
-        obj_list = pool.starmap(play, [(I_1_vars[0], I_2_vars[0], true_board_vars[0], player, move_flag),
-                                        (I_1_vars[1], I_2_vars[1], true_board_vars[0], player, move_flag),
-                                        (I_1_vars[2], I_2_vars[2], true_board_vars[0], player, move_flag),
-                                        (I_1_vars[3], I_2_vars[3], true_board_vars[0], player, move_flag)])
+    # with Pool(4) as pool:
+    #     obj_list = pool.starmap(play, [(I_1_vars[0], I_2_vars[0], true_board_vars[0], player, move_flag),
+    #                                     (I_1_vars[1], I_2_vars[1], true_board_vars[0], player, move_flag),
+    #                                     (I_1_vars[2], I_2_vars[2], true_board_vars[0], player, move_flag),
+    #                                     (I_1_vars[3], I_2_vars[3], true_board_vars[0], player, move_flag)])
 
     print('Histories: ', sum(obj_list))
