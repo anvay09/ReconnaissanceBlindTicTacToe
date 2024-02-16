4534e746df95e01496e5039293ce0164b5a9d91d
anvay09 <shah.anvay@gmail.com>
2024-02-16 17:29:33 +0530

    Fixed bug: get_histories_given_I() missing one action in history

--- test_files/test_get_histories.py
+++ test_files/test_get_histories.py
@@ -1,9 +1,11 @@
 from rbt_classes import InformationSet
 from rbt_utilties import get_histories_given_I
-
+import logging
 
 if __name__ == '__main__':
-    I = InformationSet(player='o', board=['o', '-', '-', '-', 'x', 'o', 'o', '-', 'x'])
-    move_flag = False
+    # I = InformationSet(player='o', board=['o', '-', '-', '-', 'x', 'o', 'o', '-', 'x'])
+    I = InformationSet(player='x', board=['-', '0', '0', 'x', 'x', 'o', 'o', '-', 'x'])
     H = get_histories_given_I(I)
-    print(len(H))
+    logging.info('Number of histories: {}'.format(len(H)))
+    logging.info('First 5 histories: ')
+    print(H[:5])
