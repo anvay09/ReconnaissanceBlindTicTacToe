545b7d1fb123f5ff286c6e2f50310c0c750c2da0
anvay09 <shah.anvay@gmail.com>
2024-02-18 02:54:30 +0530

    added another iteration

--- test_files/test_calc_cfr_policy_given_I.py
+++ test_files/test_calc_cfr_policy_given_I.py
@@ -31,3 +31,5 @@ if __name__ == '__main__':
     policy_obj_x, policy_obj_o, prev_regret_list = calc_cfr_policy_given_I(I, policy_obj_x, policy_obj_o, T, prev_regret_list)
     T = 1
     policy_obj_x, policy_obj_o, prev_regret_list = calc_cfr_policy_given_I(I, policy_obj_x, policy_obj_o, T, prev_regret_list)
+    T = 2
+    policy_obj_x, policy_obj_o, prev_regret_list = calc_cfr_policy_given_I(I, policy_obj_x, policy_obj_o, T, prev_regret_list)
