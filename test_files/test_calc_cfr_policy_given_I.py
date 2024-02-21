3130ca6c7aa15e4e2301c32274a81f42239bdb22
anvay09 <shah.anvay@gmail.com>
2024-02-21 17:00:55 +0530

    Updated cfr test file

--- test_files/test_calc_cfr_policy_given_I.py
+++ test_files/test_calc_cfr_policy_given_I.py
@@ -10,14 +10,11 @@ if __name__ == '__main__':
 
     # TODO: Fix Bug for root node, and for player 'o' first move
     # I = InformationSet(player='x', move_flag=True, board=['o', '0', '-', '0', 'x', '-', '-', '-', '-'])
-    I = InformationSet(player='o', move_flag=False, board=['o', '-', '-', '-', 'x', 'o', 'o', '-', 'x'])
-
-    prev_regret_list_x = [0 for _ in range(13)]
-    prev_regret_list_o = [0 for _ in range(13)]
+    # I = InformationSet(player='o', move_flag=False, board=['o', '-', '-', '-', 'x', 'o', 'o', '-', 'x'])
 
     logging.info('Initializing policy objects...')
 
-    with open('./data_files/{}.json'.format('P1_uniform_policy'), 'r') as f:
+    with open('./data_files/{}.json'.format('P1_DG_policy'), 'r') as f:
         policy_dict = json.load(f)
     policy_obj_x = Policy(player='x', policy_dict=policy_dict)
 
@@ -27,13 +24,38 @@ if __name__ == '__main__':
 
     logging.info('Loaded policy for player {}, information set {}:'.format(I.player, I.get_hash()))
 
-    if I.player == 'x':
-        print(policy_obj_x.policy_dict[I.get_hash()])
-    else:
-        print(policy_obj_o.policy_dict[I.get_hash()])
+    # if I.player == 'x':
+    #     print(policy_obj_x.policy_dict[I.get_hash()])
+    # else:
+    #     print(policy_obj_o.policy_dict[I.get_hash()])
 
+    P2_reachable_information_sets_file = 'data_files/reachable_P2_information_sets.txt'
+    P2_reachable_information_sets = set()
+    with open(P2_reachable_information_sets_file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            P2_reachable_information_sets.add(line.strip())
+
+    # prev_regret_list_x = [0 for _ in range(13)]
+    prev_regret_list_o = {I_hash:[0 for _ in range(13)] for I_hash in P2_reachable_information_sets}
+    processed_I_count = 0
+            
     for T in range(1,5):
-        policy_obj_x, policy_obj_o, prev_regret_list_x = calc_cfr_policy_given_I(I, policy_obj_x, policy_obj_o, T,
-                                                                             prev_regret_list_x)
-        print(policy_obj_x)
+        for I_hash in P2_reachable_information_sets:
+            I = InformationSet(player='o', move_flag=I_hash[-1]=='m', board=[*I_hash])
+
+            policy_obj_x, policy_obj_o, prev_regret_list_o[I_hash] = calc_cfr_policy_given_I(I, policy_obj_x, policy_obj_o, T,
+                                                                             prev_regret_list_o[I_hash])
+            
+            logging.info('Updated policy for player {}, information set {}:'.format(I.player, I.get_hash()))
+            logging.info('{}'.format(policy_obj_o.policy_dict[I_hash()]))
+            processed_I_count += 1
+            logging.info('Processed {} information sets in iteration {}...'.format(processed_I_count, T))
+        
+        processed_I_count = 0
+        logging.info('Completed iteration {}...'.format(T))
+
+    logging.info('Saving policy objects...')
+    with open('./data_files/{}.json'.format('P2_updated_policy'), 'w') as f:
+        json.dump(policy_obj_o.policy_dict, f)
     
