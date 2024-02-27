cc3eae8b140c855e61846ba609b37e7362a424cc
anvay09 <shah.anvay@gmail.com>
2024-02-27 13:45:54 +0530

    Added policy in filter_valid_h

--- test_files/filter_valid_histories_for_I.py
+++ test_files/filter_valid_histories_for_I.py
@@ -1,9 +1,13 @@
-from rbt_classes import InformationSet
+from rbt_classes import InformationSet, Policy
 from rbt_utilties import get_histories_given_I
 import json
 from tqdm import tqdm
 
 if __name__ == '__main__':
+    with open('./data_files/{}.json'.format('P1_DG_policy'), 'r') as f:
+        policy_dict = json.load(f)
+    policy_obj_x = Policy(player='x', policy_dict=policy_dict)
+
     P2_reachable_information_sets_file = 'data_files/reachable_P2_information_sets.txt'
     P2_reachable_information_sets = set()
     with open(P2_reachable_information_sets_file, 'r') as f:
@@ -16,7 +20,7 @@ if __name__ == '__main__':
     for I_hash in tqdm(P2_reachable_information_sets):
         I = InformationSet(player='o', move_flag=I_hash[-1]=='m', board=[*I_hash[:-1]])
     
-        H = get_histories_given_I(I)
+        H = get_histories_given_I(I, policy_obj_x=policy_obj_x, policy_obj_o=None)
         histories[I_hash] = H
 
     with open('data_files/valid_histories_for_reachable_I.json', 'w') as f:
