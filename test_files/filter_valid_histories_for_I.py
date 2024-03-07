d4e104f0d89475b64b901b7ab5574a6f2eebdcf4
anvay09 <shah.anvay@gmail.com>
2024-03-07 17:22:28 +0530

    Testing time to save all valid H for P2

--- test_files/filter_valid_histories_for_I.py
+++ test_files/filter_valid_histories_for_I.py
@@ -4,25 +4,25 @@ import json
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    with open('./data_files/{}.json'.format('P2_iteration_10_cfr_policy'), 'r') as f:
+    with open('./data_files/{}.json'.format('P1_DG_policy'), 'r') as f:
         policy_dict = json.load(f)
-    policy_obj_o = Policy(player='o', policy_dict=policy_dict)
+    policy_obj_x = Policy(player='x', policy_dict=policy_dict)
 
-    P1_reachable_information_sets_file = 'data_files/reachable_P1_information_sets.txt'
-    P1_reachable_information_sets = set()
-    with open(P1_reachable_information_sets_file, 'r') as f:
+    P2_reachable_information_sets_file = 'data_files/reachable_P2_information_sets.txt'
+    P2_reachable_information_sets = set()
+    with open(P2_reachable_information_sets_file, 'r') as f:
         lines = f.readlines()
         for line in lines:
-            P1_reachable_information_sets.add(line.strip())
+            P2_reachable_information_sets.add(line.strip())
 
     histories = {}
 
-    for I_hash in tqdm(P1_reachable_information_sets):
-        I = InformationSet(player='x', move_flag=I_hash[-1]=='m', board=[*I_hash[:-1]])
+    for I_hash in tqdm(P2_reachable_information_sets):
+        I = InformationSet(player='o', move_flag=I_hash[-1]=='m', board=[*I_hash[:-1]])
     
-        H = get_histories_given_I(I, policy_obj_x=None, policy_obj_o=policy_obj_o)
+        H = get_histories_given_I(I, policy_obj_x=policy_obj_x, policy_obj_o=None)
         histories[I_hash] = H
 
-    with open('data_files/p1_valid_histories_for_reachable_I.json', 'w') as f:
+    with open('data_files/p2_valid_histories_for_reachable_I.json', 'w') as f:
         json.dump(histories, f)
         
