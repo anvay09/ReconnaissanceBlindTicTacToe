037d29f96d7740bfb6cde7783e0c2e152b1f0e71
Anvay <shah.anvay@gmail.com>
2024-04-08 11:38:25 +0530

    Full tree run iteration 59

--- run_avg_prob_calc_full_tree.sh
+++ run_avg_prob_calc_full_tree.sh
@@ -1,6 +1,6 @@
 python3 ./automated_avg_prob.py --CurrentPlayer o --PolicyFileX ./data/P1_deterministic_policy.json --PolicyFileO ./data/P2_deterministic_policy.json --Round 1 --BasePath data/full_tree
 python3 ./automated_avg_prob.py --CurrentPlayer x --PolicyFileX ./data/P1_deterministic_policy.json --PolicyFileO ./data/P2_deterministic_policy.json --Round 1 --BasePath data/full_tree
-for i in {2..100}
+for i in {2..250}
 do
     python3 ./automated_avg_prob.py --CurrentPlayer o --PolicyFileX ./data/full_tree/cfr_policy/P1_iteration_$(($i-1))_cfr_policy.json --PolicyFileO ./data/full_tree/cfr_policy/P2_iteration_$(($i-1))_cfr_policy.json --Round $i --BasePath data/full_tree
     python3 ./automated_avg_prob.py --CurrentPlayer x --PolicyFileX ./data/full_tree/cfr_policy/P1_iteration_$(($i-1))_cfr_policy.json --PolicyFileO ./data/full_tree/cfr_policy/P2_iteration_$(($i-1))_cfr_policy.json --Round $i --BasePath data/full_tree
