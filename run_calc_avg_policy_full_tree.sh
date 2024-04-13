c0d02e24417ddb765372cb243059ca7bf52cf5d1
anvay09 <shah.anvay@gmail.com>
2024-04-13 08:49:03 +0530

    shell script

--- run_calc_avg_policy_full_tree.sh
+++ run_calc_avg_policy_full_tree.sh
@@ -1,7 +1,7 @@
-for i in {1..5}
+for i in {155..265}
 do
     echo "Starting Round $i..."
-    ./automated_calc_avg o data/Iterative_1/average/P2_avg_policy_after_round_{}.json $i data/Iterative_1
-    ./automated_calc_avg x data/Iterative_1/average/P1_avg_policy_after_round_{}.json $i data/Iterative_1
+    ./automated_calc_avg o data/full_tree/average/P2_avg_policy_after_round_{}.json $i data/full_tree
+    ./automated_calc_avg x data/full_tree/average/P1_avg_policy_after_round_{}.json $i data/full_tree
     echo "Finished round $i..."
 done
