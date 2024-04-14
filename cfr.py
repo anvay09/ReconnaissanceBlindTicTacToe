bfe45805e8cdb0580f6837a88d401e830c504706
anvay09 <shah.anvay@gmail.com>
2024-04-14 17:11:41 +0530

    changing starting policy

--- cfr.py
+++ cfr.py
@@ -10,8 +10,8 @@ def calc_cfr_policy_given_I_cpp(policy_file_x, policy_file_o, I_hash, player, T,
 
 if __name__ == '__main__':
     logging.info('Initializing policy objects...')
-    policy_file_x = 'data/P1_deterministic_policy.json'
-    policy_file_o = 'data/P2_deterministic_policy.json'
+    policy_file_x = 'data/P1_uniform_policy.json'
+    policy_file_o = 'data/P2_uniform_policy.json'
 
     P1_information_sets_file = 'data/P1_information_sets.txt'
     P2_information_sets_file = 'data/P2_information_sets.txt'
@@ -34,17 +34,15 @@ if __name__ == '__main__':
         next_policy_file_x = 'data/P1_iteration_{}_cfr_policy_cpp.json'.format(T)
         next_policy_file_o = 'data/P2_iteration_{}_cfr_policy_cpp.json'.format(T)
         
-        logging.info('Generating arguments...')
-        for I_hash in tqdm(P1_information_sets):
+        for I_hash in P1_information_sets:
             P1_args.append((policy_file_x, policy_file_o, I_hash, 'x', T, next_policy_file_x))
+
+        for I_hash in P2_information_sets:
+            P2_args.append((policy_file_x, policy_file_o, I_hash, 'o', T, next_policy_file_o))
 
         logging.info('Starting iteration {} for player 1...'.format(T))
         with Pool(num_workers) as p:
             p.starmap(calc_cfr_policy_given_I_cpp, tqdm(P1_args, total=len(P1_args)))
-
-    
-        for I_hash in tqdm(P2_information_sets):
-            P2_args.append((policy_file_x, policy_file_o, I_hash, 'o', T, next_policy_file_o))
 
         logging.info('Starting iteration {} for player 2...'.format(T))
         with Pool(num_workers) as p:
