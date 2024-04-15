398237f402047386157245cb4da37dd95c07a22e
anvay09 <shah.anvay@gmail.com>
2024-04-15 18:32:06 +0530

    testing boost mpi and openmp separately

--- mpitest.cpp
+++ mpitest.cpp
@@ -28,5 +28,31 @@ int main(int argc, char* argv[])
 
         std::cout << "Rank " << procid << " is processing " << i << std::endl;
     }
+
+    std::string policy_file_x = "data/P1_deterministic_policy.json";
+    std::string policy_file_o = "data/P2_deterministic_policy.json";
+    std::string P1_information_sets_file = "data/P1_information_sets.txt";
+    std::string P2_information_sets_file = "data/P2_information_sets.txt";
+    std::vector<std::string> P1_information_sets;
+    std::vector<std::string> P2_information_sets;
+
+    std::ifstream f1(P1_information_sets_file);
+    std::string line;
+    while (std::getline(f1, line)) {
+        P1_information_sets.push_back(line);
+    }
+    f1.close();
+
+    std::ifstream f2(P2_information_sets_file);
+    while (std::getline(f2, line)) {
+        P2_information_sets.push_back(line);
+    }
+    f2.close();
+
+    for (int T = 1; T <= 1000; T++) {
+        continue;
+    }
+
+    return 0;
 }
 
---
+++ openmptest.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cmath>
+#include <chrono>
+#include <ctime>
+
+int main () 
+{
+    int n=400000,  m=1000;  
+    double x=0,y=0;
+    double s=0;
+    std::vector<double> shifts(n,0);
+
+    auto start = std::chrono::system_clock::now();   
+
+    #pragma omp parallel for 
+    for (int j=0; j<n; j++) {
+
+        double r=0.0;
+        for (int i=0; i < m; i++){
+
+            double rand_g1 = cos(i/double(m));
+            double rand_g2 = sin(i/double(m));     
+
+            x += rand_g1;
+            y += rand_g2;
+            r += sqrt(rand_g1*rand_g1 + rand_g2*rand_g2);
+        }
+        shifts[j] = r / m;
+    }
+
+    std::cout << *std::max_element( shifts.begin(), shifts.end() ) << std::endl;
+
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+ 
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s"
+              << std::endl;
+    return 0;
+}
