0a56eeebe5a9174ba8f055ded811ad496fda313b
anvay09 <shah.anvay@gmail.com>
2024-06-10 17:35:56 +0530

    Plot parallel update

--- python_files/plot_parallel.py
+++ python_files/plot_parallel.py
@@ -12,10 +12,10 @@ import os
 logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
                     level=logging.INFO)
 
-def parallel_cpp(file_path_1, file_path_2, round, save_games):
-    os.system("./evaluate_policy" + " " + file_path_1 + " " + file_path_2 + " " + str(round) + " " + str(save_games))
+def parallel_cpp(file_path_1, file_path_2, round):
+    os.system("./evaluate_policy" + " " + file_path_1 + " " + file_path_2)
     
-    outfile = "round_" + str(round) + "_expected_utility.txt"
+    outfile = "expected_utility.txt"
     with open(outfile, 'r') as f:
         expected_utility = f.read()
     os.system("rm " + outfile)
@@ -37,25 +37,24 @@ if __name__ == "__main__":
     start_round = int(arguments.StartRound)
     num = int(arguments.Rounds)
     x_range = range(start_round, num)
-    args = []
+
 
     for round in x_range:
         p1_file = p1_file_base.format(str(round)) + '.json'
         p2_file = p2_file_base.format(str(round)) + '.json'
-        args.append((p1_file, p2_file, round, 0))
+        expected_utility_list.append(parallel_cpp(p1_file, p2_file, round))
 
-    with Pool(num_workers) as pool:
-        exp_util_list = pool.starmap(parallel_cpp, args)
+   
 
     X = [i for i in range(start_round, num)]
-    Y = [float(exp_util) for exp_util in exp_util_list]
+    Y = [float(exp_util) for exp_util in expected_utility_list]
     plt.plot(X, Y, color='b', label='Expected Utility')
     plt.xlabel('Round')
     plt.xticks(rotation=-60)
-    plt.yticks(np.arange(0.3, 0.65, 0.025))
+    plt.yticks(np.arange(0.2, 0.8, 0.025))
     plt.ylabel('Expected Utility')
     plt.title('Convergence of Expected Utility')
 
     plt.savefig('{}/expected_util/plots/{}.pdf'.format(arguments.BasePath, arguments.OutputFileName))
     with open('{}/expected_util/{}.json'.format(arguments.BasePath, arguments.OutputFileName), 'w') as f:
-        json.dump({"0": exp_util_list}, f)
+        json.dump({"0": expected_utility_list}, f)
