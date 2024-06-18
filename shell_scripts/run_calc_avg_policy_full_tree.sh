ec4162e0c5c5d81bc6b9b46d21f7077b8987408a
Ramsundar Anand <ramsundar.anand@gmail.com>
2024-06-18 13:41:59 +0530

    clean shell scipts, python files, unused old cpp files and data directory

--- shell_scripts/run_calc_avg_policy_full_tree.sh
+++
@@ -1,7 +0,0 @@
-for i in {155..265}
-do
-    echo "Starting Round $i..."
-    ./automated_calc_avg o data/full_tree/average/P2_avg_policy_after_round_{}.json $i data/full_tree
-    ./automated_calc_avg x data/full_tree/average/P1_avg_policy_after_round_{}.json $i data/full_tree
-    echo "Finished round $i..."
-done
--- shell_scripts/run_cfr.sh
+++
@@ -1,5 +0,0 @@
-for i in {61..100}
-do
-    python3 ./automated_cfr.py --CurrentPlayer o --PolicyFileX ./data_files/P1_cfr_policy_round_$(($i-1)).json --PolicyFileO ./data_files/P2_cfr_policy_round_$(($i-1)).json --Round $i --ReachableISFlag 1 --FilterValidHistoriesFlag 0 --BasePath data_files
-    python3 ./automated_cfr.py --CurrentPlayer x --PolicyFileX ./data_files/P1_cfr_policy_round_$(($i-1)).json --PolicyFileO ./data_files/P2_cfr_policy_round_$i.json --Round $i --ReachableISFlag 1 --FilterValidHistoriesFlag 0 --BasePath data_files
-done
