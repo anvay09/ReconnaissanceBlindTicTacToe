2cdefb8b9b5cf10d3c68a1279ddc718836b2ba03
anvay09 <shah.anvay@gmail.com>
2024-08-02 17:06:43 +0530

    print

--- cfr_iterative_new.cpp
+++ cfr_iterative_new.cpp
@@ -24,7 +24,7 @@ void run_cfr(int T, std::vector<std::string>& information_sets, std::vector<std:
         #pragma omp parallel for num_threads(number_threads) shared(regret_list, policy_obj_x, policy_obj_o)
         for (long int i = 0; i < information_sets.size(); i++) {
             std::string I_hash = information_sets[i];
-            // std::cout << "Starting iteration " << " for infoset " << i << ", hash: " << I_hash << std::endl;
+            std::cout << "Starting iteration " << T << "for infoset " << I_hash << std::endl;
 
             bool move_flag = get_move_flag(I_hash, player);
             InformationSet I(player, move_flag, I_hash);
