511f3780e3f5a5aad66ab0ef192dbacfffcf03cc
Ramsundar Anand <ramsundar.anand@gmail.com>
2024-08-10 12:23:36 +0530

    more prints

--- rbt_classes.cpp
+++ rbt_classes.cpp
@@ -780,7 +780,7 @@ std::vector< std::vector<float> > PolicyVec::read_policy_from_json(std::string& 
     
     for (json::iterator it = policy_obj.begin(); it != policy_obj.end(); ++it) {
         std::string I_hash = it.key();
-        std::cout << I_hash << std::endl;
+         std::cerr << "I_hash: " << I_hash << std::endl;
         bool move_flag;
         if (I_hash.size() != 0){
             move_flag = I_hash[I_hash.size()-1] == '|' ? true : false;
@@ -802,17 +802,23 @@ std::vector< std::vector<float> > PolicyVec::read_policy_from_json(std::string& 
         if (I_hash.back() == '_') {
             std::vector<std::string> sense_keys = {"9", "10", "11", "12"};
             for (int i = 0; i < sense_keys.size(); i++) {
+                std::cerr << "I_hash1: " << I_hash << std::endl;
+                std::cerr << "sense_keys[i]: " << sense_keys[i] << std::endl;
+                std::cerr << "policy_obj[I_hash] size: " << policy_obj[I_hash].size() << std::endl;
                 probability_distribution[stoi(sense_keys[i])] = policy_obj[I_hash][sense_keys[i]];
             }
         }
         else if (I_hash.back() == '|') {
             std::vector<std::string> move_keys = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
             for (int i = 0; i < move_keys.size(); i++) {
+                std::cerr << "I_hash1: " << I_hash << std::endl;
+                std::cerr << "move_keys[i]: " << move_keys[i] << std::endl;
+                std::cerr << "policy_obj[I_hash] size: " << policy_obj[I_hash].size() << std::endl;
                 probability_distribution[stoi(move_keys[i])] = policy_obj[I_hash][move_keys[i]];
             }
         }
         else {
-            std::cerr << "I_hash: " << I_hash << std::endl;
+            std::cerr << "I_hash1: " << I_hash << std::endl;
             if (player == 'x'){
                 std::vector<std::string> move_keys = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
                 for (int i = 0; i < move_keys.size(); i++) {
