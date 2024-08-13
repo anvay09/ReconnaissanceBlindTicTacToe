e8019c93f1df98c7aeacfdec66b1ebb405c4f7a7
anvay09 <shah.anvay@gmail.com>
2024-08-13 17:10:07 +0530

    evaluate policy compatible for v1 vs v2 and v2 vs v1

--- evaluate_policy_across_versions.cpp
+++ evaluate_policy_across_versions.cpp
@@ -2,7 +2,7 @@
 
 // g++-13 -O3 evaluate_policy_across_versions.cpp rbt_classes.cpp -o evaluate_policy_a -fopenmp
 
-double get_expected_utility(InformationSet &I_1, InformationSet &I_2, TicTacToeBoard &true_board, char player, PolicyVec &policy_obj_x, 
+double get_expected_utility_v2_vs_v1(InformationSet &I_1, InformationSet &I_2, TicTacToeBoard &true_board, char player, PolicyVec &policy_obj_x, 
                             Policy &policy_obj_o, double probability, History& current_history, char initial_player) {
     double expected_utility_h = 0.0;
     
@@ -45,9 +45,9 @@ double get_expected_utility(InformationSet &I_1, InformationSet &I_2, TicTacToeB
                 new_I.reset_zeros();
                 
                 if (player == 'x') {
-                    expected_utility_h += get_expected_utility(new_I, I_2, new_true_board, 'o', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
+                    expected_utility_h += get_expected_utility_v2_vs_v1(new_I, I_2, new_true_board, 'o', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
                 } else {
-                    expected_utility_h += get_expected_utility(I_1, new_I, new_true_board, 'x', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
+                    expected_utility_h += get_expected_utility_v2_vs_v1(I_1, new_I, new_true_board, 'x', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
                 }
             } else {
                 TerminalHistory H_T = TerminalHistory(new_history.history);
@@ -77,9 +77,9 @@ double get_expected_utility(InformationSet &I_1, InformationSet &I_2, TicTacToeB
             new_history.history.push_back(action);
 
             if (player == 'x') {
-                expected_utility_h += get_expected_utility(new_I, I_2, true_board, 'x', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
+                expected_utility_h += get_expected_utility_v2_vs_v1(new_I, I_2, true_board, 'x', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
             } else {
-                expected_utility_h += get_expected_utility(I_1, new_I, true_board, 'o', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
+                expected_utility_h += get_expected_utility_v2_vs_v1(I_1, new_I, true_board, 'o', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
             }
         }
     }
@@ -88,7 +88,7 @@ double get_expected_utility(InformationSet &I_1, InformationSet &I_2, TicTacToeB
 }
 
 
-double get_expected_utility_parallel(InformationSet &I_1, InformationSet &I_2, TicTacToeBoard &true_board, char player, PolicyVec &policy_obj_x, 
+double get_expected_utility_parallel_v2_vs_v1(InformationSet &I_1, InformationSet &I_2, TicTacToeBoard &true_board, char player, PolicyVec &policy_obj_x, 
                                      Policy &policy_obj_o, double probability, History& current_history, char initial_player) {
     double expected_utility_h = 0.0;
     std::vector<InformationSet> Depth_1_P1_Isets;
@@ -195,14 +195,14 @@ double get_expected_utility_parallel(InformationSet &I_1, InformationSet &I_2, T
 
     # pragma omp parallel for num_threads(96)
     for (int i = 0; i < Depth_1_P1_Isets.size(); i++) {
-        expected_utility_h += get_expected_utility(Depth_1_P1_Isets[i], Depth_1_P2_Isets[i], Depth_1_boards[i], Depth_1_players[i], policy_obj_x, policy_obj_o, Depth_1_probabilities[i], Depth_1_histories[i], initial_player);
+        expected_utility_h += get_expected_utility_v2_vs_v1(Depth_1_P1_Isets[i], Depth_1_P2_Isets[i], Depth_1_boards[i], Depth_1_players[i], policy_obj_x, policy_obj_o, Depth_1_probabilities[i], Depth_1_histories[i], initial_player);
     }
 
     return expected_utility_h;
 }
 
 
-double get_expected_utility_wrapper(PolicyVec& policy_obj_x, Policy& policy_obj_o){
+double get_expected_utility_wrapper_v2_vs_v1(PolicyVec& policy_obj_x, Policy& policy_obj_o){
     std::string board = "000000000";
     TicTacToeBoard true_board = TicTacToeBoard(board);
     std::string hash_1 = "";
@@ -212,7 +212,222 @@ double get_expected_utility_wrapper(PolicyVec& policy_obj_x, Policy& policy_obj_
     std::vector<int> h = {};
     TerminalHistory start_history = TerminalHistory(h);
 
-    double expected_utility = get_expected_utility_parallel(I_1, I_2, true_board, 'x', policy_obj_x, policy_obj_o, 1, start_history, 'x');
+    double expected_utility = get_expected_utility_parallel_v2_vs_v1(I_1, I_2, true_board, 'x', policy_obj_x, policy_obj_o, 1, start_history, 'x');
+    return expected_utility;
+}
+
+
+double get_expected_utility_v1_vs_v2(InformationSet &I_1, InformationSet &I_2, TicTacToeBoard &true_board, char player, Policy &policy_obj_x, 
+                            PolicyVec &policy_obj_o, double probability, History& current_history, char initial_player) {
+    double expected_utility_h = 0.0;
+    
+    InformationSet& I = player == 'x' ? I_1 : I_2;
+    // PolicyVec& policy_obj = player == 'x' ? policy_obj_x : policy_obj_o;
+    
+    std::vector<int> actions;
+    // I.get_actions_given_policy(actions, policy_obj);
+    if (player == 'x'){
+        I.get_actions_given_policy(actions, policy_obj_x);
+    }
+    else{
+        I.get_actions_given_policy(actions, policy_obj_o);
+    }
+    
+    if (I.move_flag) {
+        for (int action : actions) {
+            TicTacToeBoard new_true_board = true_board;
+            bool success = new_true_board.update_move(action, player);
+
+            if (I.get_index() == -1){
+                std::cout << "KEY ERROR: Get exp u" << std::endl;
+            }
+
+            double probability_new;
+            if (player == 'x'){
+                probability_new = probability * policy_obj_x.policy_dict[I.get_v1_hash()][action];
+            }
+            else{
+                probability_new = probability * policy_obj_o.policy_dict[I.get_index()][action];
+            }
+            // double probability_new = probability * policy_obj.policy_dict[I.get_index()][action];
+            History new_history = current_history;
+            new_history.history.push_back(action);
+            
+            char winner;
+            if (success && !new_true_board.is_win(winner) && !new_true_board.is_over()) {
+                InformationSet new_I = I;
+                new_I.update_move(action, player);
+                new_I.reset_zeros();
+                
+                if (player == 'x') {
+                    expected_utility_h += get_expected_utility_v1_vs_v2(new_I, I_2, new_true_board, 'o', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
+                } else {
+                    expected_utility_h += get_expected_utility_v1_vs_v2(I_1, new_I, new_true_board, 'x', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
+                }
+            } else {
+                TerminalHistory H_T = TerminalHistory(new_history.history);
+                H_T.set_reward();
+                if (initial_player == 'x'){
+                    expected_utility_h += H_T.reward[0] * probability_new;
+                }
+                else{
+                    expected_utility_h += H_T.reward[1] * probability_new;
+                }
+            }
+        }
+    } else {
+        for (int action : actions) {
+            InformationSet new_I = I;
+            new_I.simulate_sense(action, true_board);
+            
+            double probability_new;
+            if (player == 'x'){
+                probability_new = probability * policy_obj_x.policy_dict[I.get_v1_hash()][action];
+            }
+            else{
+                probability_new = probability * policy_obj_o.policy_dict[I.get_index()][action];
+            }
+            // double probability_new = probability * policy_obj.policy_dict[I.get_index()][action];
+            History new_history = current_history;
+            new_history.history.push_back(action);
+
+            if (player == 'x') {
+                expected_utility_h += get_expected_utility_v1_vs_v2(new_I, I_2, true_board, 'x', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
+            } else {
+                expected_utility_h += get_expected_utility_v1_vs_v2(I_1, new_I, true_board, 'o', policy_obj_x, policy_obj_o, probability_new, new_history, initial_player);
+            }
+        }
+    }
+
+    return expected_utility_h;
+}
+
+
+double get_expected_utility_parallel_v1_vs_v2(InformationSet &I_1, InformationSet &I_2, TicTacToeBoard &true_board, char player, Policy &policy_obj_x, 
+                                     PolicyVec &policy_obj_o, double probability, History& current_history, char initial_player) {
+    double expected_utility_h = 0.0;
+    std::vector<InformationSet> Depth_1_P1_Isets;
+    std::vector<InformationSet> Depth_1_P2_Isets;
+    std::vector<TicTacToeBoard> Depth_1_boards;
+    std::vector<char> Depth_1_players;
+    std::vector<double> Depth_1_probabilities;
+    std::vector<History> Depth_1_histories;
+    
+    InformationSet I = player == 'x' ? I_1 : I_2;
+    // PolicyVec policy_obj = player == 'x' ? policy_obj_x : policy_obj_o;
+
+    std::vector<int> actions;
+    if (player == 'x'){
+        I.get_actions_given_policy(actions, policy_obj_x);
+    }
+    else{
+        I.get_actions_given_policy(actions, policy_obj_o);
+    }
+
+    if (I.move_flag) {
+        for (int action : actions) {
+            TicTacToeBoard new_true_board = true_board;
+            bool success = new_true_board.update_move(action, player);
+
+            double probability_new;
+            if (player == 'x'){
+                probability_new = probability * policy_obj_x.policy_dict[I.get_v1_hash()][action];
+            }
+            else{
+                probability_new = probability * policy_obj_o.policy_dict[I.get_index()][action];
+            }
+            // double probability_new = probability * policy_obj.policy_dict[I.get_index()][action];
+            History new_history = current_history;
+            new_history.history.push_back(action);
+
+            char winner;
+            if (success && !new_true_board.is_win(winner) && !new_true_board.is_over()) {
+                InformationSet new_I = I;
+                new_I.update_move(action, player);
+                new_I.reset_zeros();
+
+                if (player == 'x') {
+                    Depth_1_P1_Isets.push_back(new_I);
+                    Depth_1_P2_Isets.push_back(I_2);
+                    Depth_1_boards.push_back(new_true_board);
+                    Depth_1_players.push_back('o');
+                    Depth_1_probabilities.push_back(probability_new);
+                    Depth_1_histories.push_back(new_history);
+                } else {
+                    Depth_1_P1_Isets.push_back(I_1);
+                    Depth_1_P2_Isets.push_back(new_I);
+                    Depth_1_boards.push_back(new_true_board);
+                    Depth_1_players.push_back('x');
+                    Depth_1_probabilities.push_back(probability_new);
+                    Depth_1_histories.push_back(new_history);
+                }
+            } else {
+                TerminalHistory H_T = TerminalHistory(new_history.history);
+                H_T.set_reward();
+                if (initial_player == 'x'){
+                    expected_utility_h += H_T.reward[0] * probability_new;
+                }
+                else{
+                    expected_utility_h += H_T.reward[1] * probability_new;
+                }
+            }
+        
+        }
+    }
+    else {
+        for (int action : actions) {
+            InformationSet new_I = I;
+            new_I.simulate_sense(action, true_board);
+
+            double probability_new;
+            if (player == 'x'){
+                probability_new = probability * policy_obj_x.policy_dict[I.get_v1_hash()][action];
+            }
+            else{
+                probability_new = probability * policy_obj_o.policy_dict[I.get_index()][action];
+            }
+            // double probability_new = probability * policy_obj.policy_dict[I.get_index()][action];
+            History new_history = current_history;
+            new_history.history.push_back(action);
+
+            if (player == 'x') {
+                Depth_1_P1_Isets.push_back(new_I);
+                Depth_1_P2_Isets.push_back(I_2);
+                Depth_1_boards.push_back(true_board);
+                Depth_1_players.push_back('x');
+                Depth_1_probabilities.push_back(probability_new);
+                Depth_1_histories.push_back(new_history);
+            } else {
+                Depth_1_P1_Isets.push_back(I_1);
+                Depth_1_P2_Isets.push_back(new_I);
+                Depth_1_boards.push_back(true_board);
+                Depth_1_players.push_back('o');
+                Depth_1_probabilities.push_back(probability_new);
+                Depth_1_histories.push_back(new_history);
+            }   
+        }
+    }
+
+    # pragma omp parallel for num_threads(96)
+    for (int i = 0; i < Depth_1_P1_Isets.size(); i++) {
+        expected_utility_h += get_expected_utility_v1_vs_v2(Depth_1_P1_Isets[i], Depth_1_P2_Isets[i], Depth_1_boards[i], Depth_1_players[i], policy_obj_x, policy_obj_o, Depth_1_probabilities[i], Depth_1_histories[i], initial_player);
+    }
+
+    return expected_utility_h;
+}
+
+
+double get_expected_utility_wrapper_v1_vs_v2(Policy& policy_obj_x, PolicyVec& policy_obj_o){
+    std::string board = "000000000";
+    TicTacToeBoard true_board = TicTacToeBoard(board);
+    std::string hash_1 = "";
+    std::string hash_2 = "";
+    InformationSet I_1 = InformationSet('x', true, hash_1);
+    InformationSet I_2 = InformationSet('o', false, hash_2);
+    std::vector<int> h = {};
+    TerminalHistory start_history = TerminalHistory(h);
+
+    double expected_utility = get_expected_utility_parallel_v1_vs_v2(I_1, I_2, true_board, 'x', policy_obj_x, policy_obj_o, 1, start_history, 'x');
     return expected_utility;
 }
 
@@ -220,6 +435,7 @@ double get_expected_utility_wrapper(PolicyVec& policy_obj_x, Policy& policy_obj_
 int main(int argc, char** argv) {
     std::string file_path_1 = argv[1];
     std::string file_path_2 = argv[2];
+    int version = std::stoi(argv[3]); // 1 for v1 vs v2, 2 for v2 vs v1
     std::vector<std::string> P1_information_sets;
     std::vector<std::string> P2_information_sets;
     std::string P1_information_sets_file = "data/P1_information_sets_V2.txt";
@@ -249,24 +465,43 @@ int main(int argc, char** argv) {
     std::cout.precision(17);
     std::cout << "Loading policies..." << std::endl;
     char player = 'x';
-    PolicyVec policy_obj_x('x', file_path_1);
-    // PolicyVec policy_obj_o('o', file_path_2);
-    Policy policy_obj_o('o', file_path_2);
-    std::cout << policy_obj_o.policy_dict.size() << std::endl;
 
-    std::cout << "Policies loaded." << std::endl;
-    std::cout << "Getting expected utility..." << std::endl;
-    auto start = std::chrono::system_clock::now();   
+    if (version == 1){
+        Policy policy_obj_x('x', file_path_1);
+        PolicyVec policy_obj_o('o', file_path_2);
+        std::cout << "Policies loaded." << std::endl;
+        std::cout << "Getting expected utility..." << std::endl;
+        auto start = std::chrono::system_clock::now();   
+        
+        double expected_utility = get_expected_utility_wrapper_v1_vs_v2(policy_obj_x, policy_obj_o);
+        std::cout << "Expected utility: " << expected_utility << std::endl;
+
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end-start;
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
     
-    double expected_utility = get_expected_utility_wrapper(policy_obj_x, policy_obj_o);
-    std::cout << "Expected utility: " << expected_utility << std::endl;
+        std::cout << "finished computation at " << std::ctime(&end_time)
+                << "elapsed time: " << elapsed_seconds.count() << "s"
+                << std::endl;
+    }
+    else if (version == 2) {
+        PolicyVec policy_obj_x('x', file_path_1);
+        Policy policy_obj_o('o', file_path_2);
+        std::cout << "Policies loaded." << std::endl;
+        std::cout << "Getting expected utility..." << std::endl;
+        auto start = std::chrono::system_clock::now();
 
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end-start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
- 
-    std::cout << "finished computation at " << std::ctime(&end_time)
-              << "elapsed time: " << elapsed_seconds.count() << "s"
-              << std::endl;
+        double expected_utility = get_expected_utility_wrapper_v2_vs_v1(policy_obj_x, policy_obj_o);
+        std::cout << "Expected utility: " << expected_utility << std::endl;
+
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end-start;
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+        std::cout << "finished computation at " << std::ctime(&end_time)
+                << "elapsed time: " << elapsed_seconds.count() << "s"
+                << std::endl; 
+    }
+
     return 0;
 }
