bf8f93c731275ae830e9f6c2d2da2604d425e63f
anvay09 <shah.anvay@gmail.com>
2024-08-21 15:58:18 +0530

    bug

--- rbt_classes.cpp
+++ rbt_classes.cpp
@@ -853,9 +853,10 @@ std::vector< std::vector<double> > PolicyVec::read_policy_from_txt(std::string& 
         int token_idx = 0;
         std::vector<std::string> tokens;
         split(line, " ", tokens);
-        // for (int j = 0; j < tokens.size(); j++) {
-        //     std::cout << tokens[j] << " ";
-        // }
+        std::cout << "Number of tokens: " << tokens.size() << std::endl;
+        for (int j = 0; j < tokens.size(); j++) {
+            std::cout << tokens[j] << " ";
+        }
         std::cout << std::endl;
         std::string I_hash = tokens[token_idx++];
         bool move_flag;
@@ -874,10 +875,10 @@ std::vector< std::vector<double> > PolicyVec::read_policy_from_txt(std::string& 
             probability_distribution[i] = 0.0;
         }
 
-        while (token_idx < tokens.size() - 1) {
-            // std::cout << "Token: " << tokens[token_idx] << std::endl;
+        while (token_idx < tokens.size()) {
+            std::cout << "Token: " << tokens[token_idx] << std::endl;
             int key = std::stoi(tokens[token_idx++]);
-            // std::cout << "Token: " << tokens[token_idx] << std::endl;
+            std::cout << "Token: " << tokens[token_idx] << std::endl;
             double value = std::stod(tokens[token_idx++]);
             probability_distribution[key] = value;
         }
