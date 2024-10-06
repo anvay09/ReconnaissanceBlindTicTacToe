4a2e5374447c26c142b2443c38b5fa8372e5943c
anvay09 <shah.anvay@gmail.com>
2024-10-06 18:09:59 +0530

    both players

--- evaluate_policy.cpp
+++ evaluate_policy.cpp
@@ -599,14 +599,14 @@ int main(int argc, char* argv[]) {
 
     std::cout << "Copying policies..." << std::endl;
     PolicyVec br_x = policy_obj_x;
-    // PolicyVec br_o = policy_obj_o;
+    PolicyVec br_o = policy_obj_o;
 
     std::cout << "Computing best response..." << std::endl;
     start = std::chrono::system_clock::now();
     expected_utility = compute_best_response_wrapper(policy_obj_x, policy_obj_o, br_x, 'x');
     std::cout << "Expected utility: " << expected_utility << std::endl;
-    // expected_utility = compute_best_response_wrapper(policy_obj_x, policy_obj_o, br_o, 'o');
-    // std::cout << "Expected utility: " << expected_utility << std::endl;
+    expected_utility = compute_best_response_wrapper(policy_obj_x, policy_obj_o, br_o, 'o');
+    std::cout << "Expected utility: " << expected_utility << std::endl;
 
     end = std::chrono::system_clock::now();
     elapsed_seconds = end-start;
@@ -616,14 +616,14 @@ int main(int argc, char* argv[]) {
               << std::endl;
 
     make_pure_strategy(br_x, 'x', P1_information_sets);
-    // make_pure_strategy(br_o, 'o', P2_information_sets);
+    make_pure_strategy(br_o, 'o', P2_information_sets);
 
     std::cout << "Getting expected utility..." << std::endl;
     start = std::chrono::system_clock::now();
     expected_utility = get_expected_utility_wrapper(br_x, policy_obj_o);
     std::cout << "Expected utility of best response against P2: " << expected_utility << std::endl;
-    // expected_utility = get_expected_utility_wrapper(policy_obj_x, br_o);
-    // std::cout << "Expected utility of best response against P1: " << expected_utility << std::endl;
+    expected_utility = get_expected_utility_wrapper(policy_obj_x, br_o);
+    std::cout << "Expected utility of best response against P1: " << expected_utility << std::endl;
 
     end = std::chrono::system_clock::now();
     elapsed_seconds = end-start;
