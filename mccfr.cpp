a59636bed9568088f8fa8f7dbe56e3f9ca668477
anvay09 <shah.anvay@gmail.com>
2024-10-29 18:04:26 +0530

    check exp u after every 1000

--- mccfr.cpp
+++ mccfr.cpp
@@ -186,7 +186,6 @@ void mccfr_outcome_sampling_best_response(PolicyVec& policy_obj, PolicyVec& best
     }
 
     for (int t = 0; t < T; t++) {
-        std::cout << "Starting iteration " << t << std::endl;
         std::vector<int> h = {};
         TerminalHistory start_history = TerminalHistory(h);
         double q_z = 0.0;
@@ -209,6 +208,7 @@ void mccfr_outcome_sampling_best_response(PolicyVec& policy_obj, PolicyVec& best
         compute_regrets_along_history(I_1, I_2, true_board, best_response, br_player, regret_list, start_history, q_z, reward, 0, 'x');        
 
         if (t % 1000 == 0) {
+            std::cout << "Finished iteration " << t << std::endl;
             double expected_utility = 0.0;
 
             if (br_player == 'x'){
