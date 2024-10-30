f4109bdec8a4ecfa256ef36ea3fe794b4f7231ac
anvay09 <shah.anvay@gmail.com>
2024-10-30 10:06:55 +0530

    bug fix

--- mccfr.cpp
+++ mccfr.cpp
@@ -760,7 +760,7 @@ void mccfr_outcome_sampling_best_response(PolicyVec& policy_obj, PolicyVec& best
 
         compute_regrets_along_history(I_1, I_2, true_board, best_response, cumulative_strategy, br_player, t, 1.0, regret_list, markers, start_history, q_z, reward, 0, 'x');        
 
-        if (t % 1000 == 0) {
+        if (t % 10000 == 0) {
             double expected_utility = 0.0;
 
             if (br_player == 'x'){
