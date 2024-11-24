99bce3dc375f61bd020725f0911fde06b6c209a4
anvay09 <shah.anvay@gmail.com>
2024-11-24 18:22:21 +0530

    trying to use reward to approximate percolated reward when percolated reward is inf

--- bandit_uct_max.cpp
+++ bandit_uct_max.cpp
@@ -371,30 +371,27 @@ double update_ucb_reverse_recursive(InformationSet& I_1, InformationSet& I_2, Ti
         if (!std::isinf(percolated_reward)){
             infoset_pull_count[I.get_index()][played_action] += 1;
             infoset_empirical_reward[I.get_index()][played_action] = (total_reward + percolated_reward) / (total_pull + 1);
-
-            std::vector<int> legal_actions;
-            I.get_actions(legal_actions);
-            double exploration_bonus = C;
-            double max_reward = -std::numeric_limits<double>::infinity();
-            for (int a : legal_actions){
-                if (infoset_pull_count[I.get_index()][a] > 0){
-                    infoset_ucb_values[I.get_index()][a] = infoset_empirical_reward[I.get_index()][a] + sqrt(exploration_bonus*log(timestep) / infoset_pull_count[I.get_index()][a]);
-
-                    if (infoset_empirical_reward[I.get_index()][a] >= max_reward){
-                        max_reward = infoset_empirical_reward[I.get_index()][a];
-                    }
-                }
-            }
-
-            return max_reward;
         }
         else {
-            return percolated_reward;
+            infoset_pull_count[I.get_index()][played_action] += 1;
+            infoset_empirical_reward[I.get_index()][played_action] = (total_reward + reward) / (total_pull + 1);
         }
-        // else {
-        // infoset_pull_count[I.get_index()][played_action] += 1;
-        // infoset_empirical_reward[I.get_index()][played_action] =  (total_reward + reward) / (total_pull + 1);
-        // }
+
+        std::vector<int> legal_actions;
+        I.get_actions(legal_actions);
+        double exploration_bonus = C;
+        double max_reward = -std::numeric_limits<double>::infinity();
+        for (int a : legal_actions){
+            if (infoset_pull_count[I.get_index()][a] > 0){
+                infoset_ucb_values[I.get_index()][a] = infoset_empirical_reward[I.get_index()][a] + sqrt(exploration_bonus*log(timestep) / infoset_pull_count[I.get_index()][a]);
+
+                if (infoset_empirical_reward[I.get_index()][a] >= max_reward){
+                    max_reward = infoset_empirical_reward[I.get_index()][a];
+                }
+            }
+        }
+
+        return max_reward;
     }
     else {
         return percolated_reward;
