30c5324b7e46fea5006b468dc2399690a2ef8d44
Ramsundar Anand <ramsundar.anand@gmail.com>
2024-11-26 12:54:51 +0530

    try overriding eps based on step size

--- mccfr.cpp
+++ mccfr.cpp
@@ -353,6 +353,8 @@ void mccfr_outcome_sampling(PolicyVec& policy_obj_x, PolicyVec& policy_obj_o, lo
     }
 
     for (long int t = 0; t <= T; t++) {
+        // overridde eps based on step size.
+        eps = 1.0/(((t*1.0)/(step_size*1.0))+1.0); 
         std::vector<int> h = {};
         TerminalHistory start_history = TerminalHistory(h);
         double q_z = 0.0;
