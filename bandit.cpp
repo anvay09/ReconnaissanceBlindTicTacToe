4071d736bb2870afe08fbb74b51d72702f416588
anvay09 <shah.anvay@gmail.com>
2024-11-30 23:34:51 +0530

    debug

--- bandit.cpp
+++ bandit.cpp
@@ -355,8 +355,14 @@ void calc_br(PolicyVec& opponent_policy, char br_player, std::vector<std::string
     std::cout << "Total number of games sampled for pulling each policy " << m << " times: " << t << std::endl;
     std::string hash = "";
     InformationSet root = br_player == 'x' ? InformationSet('x', true, hash) : InformationSet('o', false, hash);
-    build_max_policy(player_br, root, infoset_reach_count, empirical_action_reward, action_pull_count);
+    double root_value = build_max_policy(player_br, root, infoset_reach_count, empirical_action_reward, action_pull_count);
     std::cout << "Best response policy computed" << std::endl;
+    std::cout << "Value of best response policy: " << root_value << std::endl;
+    std::cout << "Value of actions at root infoset: " << std::endl;
+    for (int i = 0; i < 13; i++) {
+        std::cout << "Action " << i << ": " << player_br.policy_dict[root.get_index()][i] << std::endl;
+    }
+
     double expected_utility = 0.0;
     if (br_player == 'x') {
         expected_utility = get_expected_utility_wrapper(player_br, opponent_policy);
