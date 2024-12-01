c98e721cfa0fa8c2be9ea9f13fa1d1e22fd56a08
anvay09 <shah.anvay@gmail.com>
2024-12-01 16:07:31 +0530

    checking histogram

--- bandit.cpp
+++ bandit.cpp
@@ -53,11 +53,11 @@ void print_histogram(std::vector<long int>& visited_infosets) {
     std::cout << "Histogram of visited information sets: " << std::endl;
 
     for (int i = 0; i < 10; i++) {
-        std::cout << buckets[i] << "\t\t";
+        std::cout << buckets[i] << "\t";
     }
     std::cout << std::endl;
 
-    std::cout << "0\t\t1\t\t2\t\t3-10\t\t11-100\t\t101-1k\t\t1k-10k\t\t10k-100k\t\t100k-1M\t\t1M+" << std::endl;
+    std::cout << "0\t1\t2\t3-10\t11-100\t101-1k\t1k-10k\t10k-100k\t100k-1M\t1M+" << std::endl;
 }
 
 
