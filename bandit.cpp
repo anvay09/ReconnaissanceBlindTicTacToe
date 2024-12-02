177885e55f963ab19ce9839e7a74b794d995a68a
anvay09 <shah.anvay@gmail.com>
2024-12-02 11:41:16 +0530

    using inf again

--- bandit.cpp
+++ bandit.cpp
@@ -298,7 +298,7 @@ double build_max_policy(PolicyVec& policy_obj, InformationSet& I, std::vector<lo
     std::vector<int> legal_actions;
     I.get_actions(legal_actions);
     std::vector<double> action_values(13, 0.0);
-    double infoset_value = -1.0;
+    double infoset_value = - std::numeric_limits<double>::infinity();
  
     for (int a : legal_actions){
         std::unordered_set<std::string> cohort;
@@ -311,14 +311,16 @@ double build_max_policy(PolicyVec& policy_obj, InformationSet& I, std::vector<lo
             InformationSet I_prime(I.player, get_move_flag(I_prime_hash, I.player), I_prime_hash);
             cohort_values[I_prime_hash] = build_max_policy(policy_obj, I_prime, infoset_reach_count, empirical_action_reward, action_pull_count);
 
-            norm += infoset_reach_count[I_prime.get_index()];
-            action_values[a] += cohort_values[I_prime_hash] * infoset_reach_count[I_prime.get_index()];
+            if (!std::isnan(cohort_values[I_prime_hash])){
+                norm += infoset_reach_count[I_prime.get_index()];
+                action_values[a] += cohort_values[I_prime_hash] * infoset_reach_count[I_prime.get_index()];
+            }
         }
 
         norm += pull_count;
         action_values[a] += empirical_action_reward[I.get_index()][a];
         if (norm == 0){
-            action_values[a] = -1.0;
+            action_values[a] = - std::numeric_limits<double>::infinity();
         }
         else{
             action_values[a] /= norm;
@@ -330,7 +332,7 @@ double build_max_policy(PolicyVec& policy_obj, InformationSet& I, std::vector<lo
         }
     }
 
-    if (fabs(infoset_value + 1.0) < 1e-6){
+    if (std::isnan(infoset_value)){
         // uniform policy
         for (int a : legal_actions){
             policy_obj.policy_dict[I.get_index()][a] = 1.0/legal_actions.size();
@@ -346,7 +348,7 @@ double build_max_policy(PolicyVec& policy_obj, InformationSet& I, std::vector<lo
 
         // update policy
         for (int a : legal_actions){
-            if (action_values[a] == infoset_value){
+            if (fabs(action_values[a] - infoset_value) < 1e-6){
                 policy_obj.policy_dict[I.get_index()][a] = 1.0/count;
             }
             else{
@@ -355,15 +357,15 @@ double build_max_policy(PolicyVec& policy_obj, InformationSet& I, std::vector<lo
         }
     }
 
-    std::cout << "Building max policy for information set: " << I.get_hash() << std::endl;
-    for (int a : legal_actions){
-        std::cout << "Action: " << a << " Value: " << action_values[a] << std::endl;
-    }
-    std::cout << "Information set value: " << infoset_value << std::endl;
-    std::cout << "Policy: " << std::endl;
-    for (int a : legal_actions){
-        std::cout << "Action: " << a << " Probability: " << policy_obj.policy_dict[I.get_index()][a] << std::endl;
-    }
+    // std::cout << "Building max policy for information set: " << I.get_hash() << std::endl;
+    // for (int a : legal_actions){
+    //     std::cout << "Action: " << a << " Value: " << action_values[a] << std::endl;
+    // }
+    // std::cout << "Information set value: " << infoset_value << std::endl;
+    // std::cout << "Policy: " << std::endl;
+    // for (int a : legal_actions){
+    //     std::cout << "Action: " << a << " Probability: " << policy_obj.policy_dict[I.get_index()][a] << std::endl;
+    // }
 
     return infoset_value;
 }
