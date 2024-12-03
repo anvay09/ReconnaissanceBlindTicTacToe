d5815ddc5b52094dbca53e060aa3c87e16f00016
anvay09 <shah.anvay@gmail.com>
2024-12-03 22:40:10 +0530

    trying mixed policy again

--- bandit.cpp
+++ bandit.cpp
@@ -571,6 +571,12 @@ void calc_br(PolicyVec& opponent_policy, char br_player, std::vector<std::string
         double root_value = build_max_policy_parallel(player_br, root, infoset_reach_count, empirical_action_reward, action_pull_count);
         std::cout << "Best response policy computed" << std::endl;
 
+        std::cout << "Probability distribution of root information set: " << std::endl;
+        for (int i = 0; i < 13; i++) {
+            std::cout << player_br.policy_dict[root.get_index()][i] << " ";
+        }
+        std::cout << std::endl;
+
         double expected_utility = 0.0;
         if (br_player == 'x') {
             expected_utility = get_expected_utility_wrapper(player_br, opponent_policy);
