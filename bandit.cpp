027da09c9a3d2dbe9675228c7c38886a885246a5
anvay09 <shah.anvay@gmail.com>
2024-12-05 18:30:12 +0530

    trying hyperparams

--- bandit.cpp
+++ bandit.cpp
@@ -583,7 +583,7 @@ void calc_br(PolicyVec& opponent_policy, char br_player, std::vector<std::string
         double root_value = build_max_policy_parallel(player_br, root, infoset_reach_count, empirical_action_reward, action_pull_count);
         std::cout << "Best response policy computed" << std::endl;
 
-        if (j % 100 == 0 && j != 0) { 
+        if (j % 10 == 0 && j != 0) { 
             double expected_utility = 0.0;
             if (br_player == 'x') {
                 expected_utility = get_expected_utility_wrapper(player_br, opponent_policy);
