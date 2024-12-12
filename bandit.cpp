2f0f1807a58c2925256d033432de8ffed0dd9fe8
anvay09 <shah.anvay@gmail.com>
2024-12-12 18:23:47 +0530

    checking UCB values of root

--- bandit.cpp
+++ bandit.cpp
@@ -924,7 +924,9 @@ double build_max_UCB_policy_parallel(PolicyVec& policy_obj, InformationSet& I, s
         }
     }
 
+    std::cout << "Action UCB values: ";
     for (int a : legal_actions){
+        std::cout << action_ucb_values[a] << " ";
         if (action_ucb_values[a] > max_ucb){
             max_ucb = action_ucb_values[a];
         }
@@ -1215,7 +1217,11 @@ void calc_br(PolicyVec& opponent_policy, char br_player, std::vector<std::string
     std::cout << "Max UCB policy computed" << std::endl;
 
     for (int j = 0; j < iterations; j++) {
-        if (j % 100 == 0 && j != 0) { 
+        if (j % log_frequency == 0 && j != 0) { 
+            root = br_player == 'x' ? InformationSet('x', true, hash) : InformationSet('o', false, hash);
+            std::vector<int> success_metrics{0, 0, 0};
+            double max_UCB = build_max_UCB_policy_parallel(player_max_ucb_policy, root, infoset_reach_count, I_tickmark, empirical_action_reward, action_terminal_reach_count, infoset_time_step, C, success_metrics, infoset_ucb_values, success_metrics_pi_hat, action_explore_count);
+
             double expected_utility = 0.0;
             if (br_player == 'x') {
                 expected_utility = get_expected_utility_wrapper(player_br, opponent_policy);
