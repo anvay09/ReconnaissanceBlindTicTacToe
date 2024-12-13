85c5013b2303050585b55731309b0cd44bf93dae
anvay09 <shah.anvay@gmail.com>
2024-12-13 23:23:21 +0530

    trying to average max policy

--- bandit.cpp
+++ bandit.cpp
@@ -617,7 +617,7 @@ double build_max_reward_policy_parallel(PolicyVec& policy_obj, InformationSet&I,
 
 void update_max_reward_policy_given_history(InformationSet& I, TicTacToeBoard& true_board, InformationSet& opponent_I, History& game, PolicyVec& max_reward_policy, 
                                             std::vector<int>& infoset_reach_count, std::vector<std::vector<std::vector<int>>>& empirical_action_reward, 
-                                            std::vector<std::vector<int>>& action_terminal_reach_count, std::vector<double>& infoset_values, char curr_player, char br_player, int traversal_index) {
+                                            std::vector<std::vector<int>>& action_terminal_reach_count, std::vector<double>& infoset_values, char curr_player, char br_player, int traversal_index, PolicyVec& average_policy) {
     if (traversal_index == game.history.size()){
         return;
     }
@@ -631,13 +631,13 @@ void update_max_reward_policy_given_history(InformationSet& I, TicTacToeBoard& t
             new_I.update_move(action, I.player);
             new_I.reset_zeros();
             new_board.update_move(action, I.player);
-            update_max_reward_policy_given_history(new_I, new_board, opponent_I, game, max_reward_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, toggle_player(curr_player), br_player, traversal_index + 1);
+            update_max_reward_policy_given_history(new_I, new_board, opponent_I, game, max_reward_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, toggle_player(curr_player), br_player, traversal_index + 1, average_policy);
         }
         else {
             InformationSet new_I = I;
             new_I.simulate_sense(action, true_board);
             I.reset_zeros();
-            update_max_reward_policy_given_history(new_I, true_board, opponent_I, game, max_reward_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, curr_player, br_player, traversal_index + 1);
+            update_max_reward_policy_given_history(new_I, true_board, opponent_I, game, max_reward_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, curr_player, br_player, traversal_index + 1, average_policy);
         }
 
         std::vector<int> legal_actions;
@@ -703,13 +703,17 @@ void update_max_reward_policy_given_history(InformationSet& I, TicTacToeBoard& t
             // sample from candidate actions
             int action = candidate_actions[std::rand() % candidate_actions.size()];
             std::vector<double>& prob_dist = max_reward_policy.policy_dict[I.get_index()];
+            std::vector<double>& average_prob_dist = average_policy.policy_dict[I.get_index()];
+            double t = (double) infoset_reach_count[I.get_index()];
 
             for (int a : legal_actions){
                 if (a == action){
                     prob_dist[a] = 1.0;
+                    average_prob_dist[a] = (average_prob_dist[a] * (t - 1.0) + 1.0) / t;
                 }
                 else{
                     prob_dist[a] = 0.0;
+                    average_prob_dist[a] = (average_prob_dist[a] * (t - 1.0)) / t;
                 }
             }
 
@@ -721,11 +725,11 @@ void update_max_reward_policy_given_history(InformationSet& I, TicTacToeBoard& t
             opponent_I.update_move(action, opponent_I.player);
             opponent_I.reset_zeros();
             true_board.update_move(action, opponent_I.player);
-            update_max_reward_policy_given_history(I, true_board, opponent_I, game, max_reward_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, toggle_player(curr_player), br_player, traversal_index + 1);
+            update_max_reward_policy_given_history(I, true_board, opponent_I, game, max_reward_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, toggle_player(curr_player), br_player, traversal_index + 1, average_policy);
         }
         else {
             opponent_I.simulate_sense(action, true_board);
-            update_max_reward_policy_given_history(I, true_board, opponent_I, game, max_reward_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, curr_player, br_player, traversal_index + 1);
+            update_max_reward_policy_given_history(I, true_board, opponent_I, game, max_reward_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, curr_player, br_player, traversal_index + 1, average_policy);
         }
     }
 }
@@ -1169,15 +1173,20 @@ void update_max_UCB_policy_given_history(InformationSet& I, TicTacToeBoard& true
 void calc_br(PolicyVec& opponent_policy, char br_player, std::vector<std::string>& player_information_sets, int log_frequency, int m, PolicyVec& player_br, int experiment_number) {
     std::vector<std::vector<int>> I_a_tickmark(player_information_sets.size(), std::vector<int>(13, 0));
     std::vector<int> I_tickmark(player_information_sets.size(), 0);
+
     std::vector<int> infoset_reach_count(player_information_sets.size(), 0);
     std::vector<int> infoset_time_step(player_information_sets.size(), 1);
     std::vector<double> infoset_values(player_information_sets.size(), 0.0);
     std::vector<double> infoset_ucb_values(player_information_sets.size(), 0.0);
+
     std::vector<std::vector<int>> success_metrics_pi_hat(player_information_sets.size(), std::vector<int>(3, 0));
+
     std::vector<std::vector<std::vector<int>>> empirical_action_reward(player_information_sets.size(), std::vector<std::vector<int>>(13, std::vector<int>(3, 0)));
     std::vector<std::vector<int>> action_terminal_reach_count(player_information_sets.size(), std::vector<int>(13, 0));
     std::vector<std::vector<int>> action_explore_count(player_information_sets.size(), std::vector<int>(13, 0));
+
     PolicyVec player_max_ucb_policy(br_player, player_information_sets);
+    PolicyVec player_average_br(br_player, player_information_sets);
     std::vector<std::pair<int, double>> exploitability_log; 
 
     int flag = 1;
@@ -1226,10 +1235,12 @@ void calc_br(PolicyVec& opponent_policy, char br_player, std::vector<std::string
     double root_val = build_max_reward_policy_parallel(player_br, root, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values);
     std::cout << "Best response policy computed" << std::endl;
 
-    // root = br_player == 'x' ? InformationSet('x', true, hash) : InformationSet('o', false, hash);
-    // std::vector<int> success_metrics{0, 0, 0};
-    // double max_UCB = build_max_UCB_policy_parallel(player_max_ucb_policy, root, infoset_reach_count, I_tickmark, empirical_action_reward, action_terminal_reach_count, infoset_time_step, C, success_metrics, infoset_ucb_values, success_metrics_pi_hat, action_explore_count);
-    // std::cout << "Max UCB policy computed" << std::endl;
+    player_average_br = player_br;
+
+    root = br_player == 'x' ? InformationSet('x', true, hash) : InformationSet('o', false, hash);
+    std::vector<int> success_metrics{0, 0, 0};
+    double max_UCB = build_max_UCB_policy_parallel(player_max_ucb_policy, root, infoset_reach_count, I_tickmark, empirical_action_reward, action_terminal_reach_count, infoset_time_step, C, success_metrics, infoset_ucb_values, success_metrics_pi_hat, action_explore_count);
+    std::cout << "Max UCB policy computed" << std::endl;
 
     for (int j = 0; j <= iterations; j++) {
         if (j % 1000 == 0 && j != 0) { 
@@ -1242,11 +1253,13 @@ void calc_br(PolicyVec& opponent_policy, char br_player, std::vector<std::string
             int sample_count = (j + 1) * samples * 2 + t;
 
             if (br_player == 'x') {
-                expected_utility = get_expected_utility_wrapper(player_br, opponent_policy);
+                // expected_utility = get_expected_utility_wrapper(player_br, opponent_policy);
+                expected_utility = get_expected_utility_wrapper(player_average_br, opponent_policy);
                 exploitability_log.push_back(std::make_pair(sample_count, exact_br_value - expected_utility));
             } 
             else {
-                expected_utility = get_expected_utility_wrapper(opponent_policy, player_br);
+                // expected_utility = get_expected_utility_wrapper(opponent_policy, player_br);
+                expected_utility = get_expected_utility_wrapper(opponent_policy, player_average_br);
                 exploitability_log.push_back(std::make_pair(sample_count, exact_br_value - expected_utility));
             }
             std::cout << "Expected utility of best response policy: " << expected_utility << std::endl;
@@ -1274,15 +1287,15 @@ void calc_br(PolicyVec& opponent_policy, char br_player, std::vector<std::string
             std::string hash_2 = "";
             InformationSet I_1 = InformationSet('x', true, hash_1);
             InformationSet I_2 = InformationSet('o', false, hash_2);
-            update_max_reward_policy_given_history(I_1, true_board, I_2, start_history, player_br, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, 'x', br_player, 0);
+            update_max_reward_policy_given_history(I_1, true_board, I_2, start_history, player_br, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, 'x', br_player, 0, player_average_br);
 
-            // board = "000000000";
-            // true_board = TicTacToeBoard(board);
-            // hash_1 = "";
-            // hash_2 = "";
-            // I_1 = InformationSet('x', true, hash_1);
-            // I_2 = InformationSet('o', false, hash_2);
-            // update_max_UCB_policy_given_history(I_1, true_board, I_2, start_history, player_max_ucb_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_ucb_values, 'x', br_player, 0, infoset_time_step, C, I_tickmark, success_metrics_pi_hat, action_explore_count);
+            board = "000000000";
+            true_board = TicTacToeBoard(board);
+            hash_1 = "";
+            hash_2 = "";
+            I_1 = InformationSet('x', true, hash_1);
+            I_2 = InformationSet('o', false, hash_2);
+            update_max_UCB_policy_given_history(I_1, true_board, I_2, start_history, player_max_ucb_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_ucb_values, 'x', br_player, 0, infoset_time_step, C, I_tickmark, success_metrics_pi_hat, action_explore_count);
 
             h = {};
             start_history = TerminalHistory(h);
@@ -1294,20 +1307,20 @@ void calc_br(PolicyVec& opponent_policy, char br_player, std::vector<std::string
             hash_2 = "";
             I_1 = InformationSet('x', true, hash_1);
             I_2 = InformationSet('o', false, hash_2);
-            update_max_reward_policy_given_history(I_1, true_board, I_2, start_history, player_max_ucb_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, 'x', br_player, 0);
+            update_max_reward_policy_given_history(I_1, true_board, I_2, start_history, player_max_ucb_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_values, 'x', br_player, 0, player_average_br);
 
-            // board = "000000000";
-            // true_board = TicTacToeBoard(board);
-            // hash_1 = "";
-            // hash_2 = "";
-            // I_1 = InformationSet('x', true, hash_1);
-            // I_2 = InformationSet('o', false, hash_2);
-            // update_max_UCB_policy_given_history(I_1, true_board, I_2, start_history, player_max_ucb_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_ucb_values, 'x', br_player, 0, infoset_time_step, C, I_tickmark, success_metrics_pi_hat, action_explore_count);
+            board = "000000000";
+            true_board = TicTacToeBoard(board);
+            hash_1 = "";
+            hash_2 = "";
+            I_1 = InformationSet('x', true, hash_1);
+            I_2 = InformationSet('o', false, hash_2);
+            update_max_UCB_policy_given_history(I_1, true_board, I_2, start_history, player_max_ucb_policy, infoset_reach_count, empirical_action_reward, action_terminal_reach_count, infoset_ucb_values, 'x', br_player, 0, infoset_time_step, C, I_tickmark, success_metrics_pi_hat, action_explore_count);
         } 
     }
 
     std::cout << "Saving exploitability log" << std::endl;
-    std::string file_name = "data/LUCB_uniform_exploitability_log_" + std::to_string(experiment_number) + ".txt";
+    std::string file_name = "data/LUCB_average_exploitability_log_" + std::to_string(experiment_number) + ".txt";
 
     std::ofstream f(file_name);
     for (int i = 0; i < exploitability_log.size(); i++) {
@@ -1362,12 +1375,12 @@ int main(int argc, char* argv[]) {
         int log_frequency = 10000;
         char player = 'x';
         int m = 1;
-        // std::cout << "Enter log frequency: ";
-        // std::cin >> log_frequency;
-        // std::cout << "Enter player for pull arms: ";
-        // std::cin >> player;
-        // std::cout << "Enter value of m: ";
-        // std::cin >> m;
+        std::cout << "Enter log frequency: ";
+        std::cin >> log_frequency;
+        std::cout << "Enter player for pull arms: ";
+        std::cin >> player;
+        std::cout << "Enter value of m: ";
+        std::cin >> m;
 
         if (player == 'x') {
             PolicyVec uniform_x('x', P1_information_sets);
--- plot_exp.py
+++ plot_exp.py
@@ -43,7 +43,7 @@ y = y[12:401]
 y = [y[i] / num_experiments for i in range(len(y))]
 
 # LUCB uniform data
-num_experiments = 64
+num_experiments = 88
 x_uniform = [1000 * i for i in range(0, 412)]
 y_uniform = [-1.0 for i in range(0, 412)]
 
@@ -71,7 +71,7 @@ x_uniform = x_uniform[12:401]
 y_uniform = y_uniform[12:401]
 y_uniform = [y_uniform[i] / num_experiments for i in range(len(y_uniform))]
 
-# mcfr data
+# mccfr data
 num_experiments = 4
 x_mcfr = [1000 * i for i in range(0, 412)]
 y_mcfr = [0.0 for i in range(0, 412)]
@@ -87,13 +87,34 @@ x_mcfr = x_mcfr[12:401]
 y_mcfr = y_mcfr[12:401]
 y_mcfr = [y_mcfr[i] / num_experiments for i in range(len(y_mcfr))]
 
+
+# mccfr damnpened data
+num_experiments = 15
+x_mcfr_dampened = [1000 * i for i in range(0, 412)]
+y_mcfr_dampened = [0.0 for i in range(0, 412)]
+
+for i in range(1, num_experiments + 1):
+    iterations, exploitabilities = read_exploitability_log(f"data/mccfr_dampen_eps_exploitability_log_{i}.txt")
+    for j in range(411):
+        x_index = iterations[j]
+        x_index = iterations[j] // 1000
+        y_mcfr_dampened[x_index] += exploitabilities[j]
+
+x_mcfr_dampened = x_mcfr_dampened[12:401]
+y_mcfr_dampened = y_mcfr_dampened[12:401]
+y_mcfr_dampened = [y_mcfr_dampened[i] / num_experiments for i in range(len(y_mcfr_dampened))]
+
+
 # plot LUCB uniform data, yellow color
-plt.plot(x_uniform, y_uniform, marker='', linewidth=1, color='blue', label='Exploitability of LUCB with uniform exploration against number of samples, averaged over 64 experiments')
+plt.plot(x_uniform, y_uniform, marker='', linewidth=1, color='blue', linestyle = '--', label='Exploitability of LUCB with uniform exploration against number of samples, averaged over 88 experiments')
 # plot x vs y, join points with lines, no markers, line width 1, color blue, label "Exploitability of LUCB BR against number of samples, averaged over 5 experiments"
 plt.plot(x, y, marker='', linewidth=1, color='black', label='Exploitability of LUCB BR against number of samples, averaged over 100 experiments')
 # plot mccfr data, red color
 plt.plot(x_mcfr, y_mcfr, marker='', linewidth=1, color='red', label='Exploitability of MCCFR against number of samples, averaged over 4 experiments')
+# plot mccfr dampened data, green color
+plt.plot(x_mcfr_dampened, y_mcfr_dampened, marker='', linewidth=1, color='green', label='Exploitability of MCCFR with dampened epsilon against number of samples, averaged over 15 experiments')
 
+plt.yticks([0.01, 0.05, 0.1, 0.2, 0.3, 0.5])
 plt.xlabel('Number of samples')
 plt.ylabel('Exploitability')
 plt.legend()
