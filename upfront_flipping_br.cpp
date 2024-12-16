39ba1bfcae75d910449a1e5b4e511d7a3cf80f74
Ramsundar Anand <ramsundar.anand@gmail.com>
2024-12-16 17:35:20 +0530

    bug fix

--- upfront_flipping_br.cpp
+++ upfront_flipping_br.cpp
@@ -159,9 +159,9 @@ double compute_regrets_along_history(InformationSet& I_1, InformationSet& I_2, T
                 }
                 //regret_I[actions[i]] += (reward * reach_prob * (1 - played_action_prob)) / q_z;
             } 
-            else {
-                regret_I[actions[i]] += -reward * reach_prob * played_action_prob / q_z;
-            }
+            // else {
+            //     regret_I[actions[i]] += -reward * reach_prob * played_action_prob / q_z;
+            // }
 
             cumulative_prob_table[actions[i]] += (t - markers[I.get_index()]) * br_prob_dist[actions[i]] * forward_reach;
             regret_sum += regret_I[actions[i]] > 0 ? regret_I[actions[i]] : 0;
