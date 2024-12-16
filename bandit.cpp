061ce191b4b516269d305b1140d06dee13cdb2d8
anvay09 <shah.anvay@gmail.com>
2024-12-16 14:01:20 +0530

    debug

--- bandit.cpp
+++ bandit.cpp
@@ -226,13 +226,13 @@ int explore(InformationSet& I_1, InformationSet& I_2, InformationSet previous_op
             // update action pull count and empirical action reward only when action leads to terminal state
             if (curr_player == br_player){
                 action_terminal_reach_count[I.get_index()][action] += 1;
-                if (r == 1.0){
+                if (fabs(r - 1.0) < 1e-6){
                     empirical_action_reward[I.get_index()][action][0] += 1;
                 }
-                else if (r == 0.0){
+                else if (fabs(r - 0.0) < 1e-6){
                     empirical_action_reward[I.get_index()][action][1] += 1;
                 }
-                else if (r == -1.0){
+                else if (fabs(r + 1.0) < 1e-6){
                     empirical_action_reward[I.get_index()][action][2] += 1;
                 }
             }
@@ -241,13 +241,13 @@ int explore(InformationSet& I_1, InformationSet& I_2, InformationSet previous_op
                 int third_last_action = current_history.history[current_history.history.size() - 3];
                 action_terminal_reach_count[previous_opponent_I.get_index()][third_last_action] += 1;
 
-                if (r == 1.0){
+                if (fabs(r - 1.0) < 1e-6){
                     empirical_action_reward[previous_opponent_I.get_index()][third_last_action][0] += 1;
                 }
-                else if (r == 0.0){
+                else if (fabs(r - 0.0) < 1e-6){
                     empirical_action_reward[previous_opponent_I.get_index()][third_last_action][1] += 1;
                 }
-                else if (r == -1.0){
+                else if (fabs(r + 1.0) < 1e-6){
                     empirical_action_reward[previous_opponent_I.get_index()][third_last_action][2] += 1;
                 }
             }
@@ -356,13 +356,13 @@ int exploit(InformationSet& I_1, InformationSet& I_2, InformationSet previous_op
             // update action pull count and empirical action reward only when action leads to terminal state
             if (curr_player == br_player){
                 action_terminal_reach_count[I.get_index()][action] += 1;        
-                if (r == 1.0){
+                if (fabs(r - 1.0) < 1e-6){
                     empirical_action_reward[I.get_index()][action][0] += 1;
                 }
-                else if (r == 0.0){
+                else if (fabs(r - 0.0) < 1e-6){
                     empirical_action_reward[I.get_index()][action][1] += 1;
                 }
-                else if (r == -1.0){
+                else if (fabs(r + 1.0) < 1e-6){
                     empirical_action_reward[I.get_index()][action][2] += 1;
                 }
             }
@@ -371,13 +371,13 @@ int exploit(InformationSet& I_1, InformationSet& I_2, InformationSet previous_op
                 int third_last_action = current_history.history[current_history.history.size() - 3];
                 action_terminal_reach_count[previous_opponent_I.get_index()][third_last_action] += 1;
                 
-                if (r == 1.0){
+                if (fabs(r - 1.0) < 1e-6){
                     empirical_action_reward[previous_opponent_I.get_index()][third_last_action][0] += 1;
                 }
-                else if (r == 0.0){
+                else if (fabs(r - 0.0) < 1e-6){
                     empirical_action_reward[previous_opponent_I.get_index()][third_last_action][1] += 1;
                 }
-                else if (r == -1.0){
+                else if (fabs(r + 1.0) < 1e-6){
                     empirical_action_reward[previous_opponent_I.get_index()][third_last_action][2] += 1;
                 }
             }
