a137d1ef0431d419555eb629dd14fc65441e7fde
Ramsundar Anand <ramsundar.anand@gmail.com>
2025-01-12 09:02:36 +0530

    Add poker classes and utilities on IJCAI branch + add count terminal histories for Sandholm's algorithm

---
+++ cpp_headers/poker_classes.hpp
@@ -0,0 +1,142 @@
+#ifndef POKER_CLASSES_HPP
+#define POKER_CLASSES_HPP
+
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <iterator>
+#include <utility>
+#include <tuple>
+#include <cassert>
+#include <chrono>
+#include <ctime>
+
+static std::string EMPTY_TABLE = "---"; // Player 1 card {J, Q, K}, Player 2 card {J, Q, K}, Board card {J, Q, K}
+static std::string EMPTY_HASH = "";
+static double LEDUC_MIN_UTILITY = -13.0;
+static double LEDUC_MAX_UTILITY = 13.0;
+static double KUHN_MIN_UTILITY = -2.0;
+static double KUHN_MAX_UTILITY = 2.0;
+
+static std::vector<std::string> unique_draws_leduc = {"JJQ", "JQJ", "QJJ", "QQJ", "QJQ", "JQQ", 
+                                                      "KKJ", "KJK", "JKK", "KKQ", "KQK", "QKK", 
+                                                      "QQK", "QKQ", "KQQ", "JJK", "JKJ", "KJJ",
+                                                      "JQK", "JKQ", "QJK", "QKJ", "KJQ", "KQJ"};
+static double p = 1.0/30.0;
+static std::vector<double> draw_probabilities_leduc = {p, p, p, p, p, p,
+                                                       p, p, p, p, p, p,
+                                                       p, p, p, p, p, p,
+                                                       2*p, 2*p, 2*p, 2*p, 2*p, 2*p};
+
+static std::vector<std::string> unique_draws_kuhn = {"JQK", "JKQ", "QJK", "QKJ", "KJQ", "KQJ"};
+static std::vector<double> draw_probabilities_kuhn = {1.0/6.0, 1.0/6.0, 1.0/6.0, 1.0/6.0, 1.0/6.0, 1.0/6.0};
+
+class Policy;
+class PolicyVec;
+
+class PokerTable
+{
+public:
+    std::string cards;
+    std::string bid_sequence;
+    char player_to_move;
+    char game;
+    PokerTable(std::string& cards = EMPTY_TABLE, std::string& bid_sequence = EMPTY_HASH, char player = 'x', char game = 'L');
+    char operator[](int key) const;
+    char & operator[](int key);
+    void operator=(const PokerTable &other);
+    bool operator==(const PokerTable &other);
+    PokerTable copy();
+    bool is_win(char& winner);
+    bool is_over();
+    bool is_draw();
+    bool is_valid_move(int action);
+    bool update_move(int action);
+    void print_cards();
+};
+
+class InformationSet : public PokerTable
+{
+public:
+    static std::unordered_map<std::string, int > P1_hash_to_int_map;
+    static std::unordered_map<std::string, int > P2_hash_to_int_map;
+
+    char player;
+    bool move_flag;
+    std::string hash;
+    int index;
+    InformationSet();
+    InformationSet(char player, bool move_flag, std::string& hash, std::string& cards, char game = 'L');
+    InformationSet(char player, bool move_flag, std::string& hash, std::string& cards, int index, char game = 'L');
+    InformationSet(char player, bool move_flag, std::string& hash = EMPTY_HASH, char game = 'L');
+    bool operator==(const InformationSet &other);
+    char other_player();
+    InformationSet copy();
+    std::string get_hash();
+    std::string get_cards_from_hash();
+    int get_index();
+    void get_states(std::vector<PokerTable> &states);
+    void get_actions(std::vector<int> &actions);
+    void get_actions_given_policy(std::vector<int>& actions, PolicyVec& policy_obj);
+    void get_actions_given_policy(std::vector<int>& actions, Policy& policy_obj);
+    void get_valid_moves(std::vector<int> &actions);
+    void simulate_sense(int action, PokerTable& true_cards);
+    void get_useful_senses(std::vector<int> &actions);
+    bool is_valid_move(int action);
+    bool update_move(int action);
+    bool is_over();
+};
+
+class History
+{
+public:
+    std::vector<int> history;
+    int track_traversal_index;
+    History(std::vector<int>& history);
+    char other_player(char player);
+    std::vector<double> update_true_cards_given_history(PokerTable &true_cards);
+    void get_information_sets(InformationSet& I_1, InformationSet& I_2);
+    void print_history();
+};
+
+class TerminalHistory : public History
+{
+public:
+    std::vector<double> reward;
+    TerminalHistory(std::vector<int>& history, std::vector<double> reward = {0.0, 0.0});
+    TerminalHistory copy();
+    void set_reward(char game);
+};
+
+class NonTerminalHistory : public History
+{
+public:
+    NonTerminalHistory(std::vector<int>& history);
+    NonTerminalHistory copy();
+};
+
+// similar to python's split method
+size_t split(const std::string &txt, std::vector<std::string> &strs, char ch);
+
+class PolicyVec
+{
+public:
+    char player;
+    std::vector<std::vector<double> > policy_dict;
+    PolicyVec();
+    PolicyVec(char player, std::vector<std::string> & information_sets, char game);
+    PolicyVec(char player, std::string& file_path, char game);
+    PolicyVec(char player, std::string& file_path, char game, bool from_txt);
+    PolicyVec(char player, std::vector<std::vector<double> >& policy_dict);
+    PolicyVec copy();
+    std::vector<std::vector<double> > read_policy_from_json(std::string& file_path, char player, char game);
+    std::vector<std::vector<double> > read_policy_from_txt(std::string& file_path, char player, char game);
+};
+
+#endif // POKER_CLASSES_HPP_
---
+++ cpp_headers/poker_utilities.hpp
@@ -0,0 +1,51 @@
+#include "poker_classes.hpp"
+#include "json.hpp"
+using json = nlohmann::json;
+
+char toggle_player(char player);
+
+void get_states_in_infoset(InformationSet &I, std::vector<PokerTable> &states);
+
+void get_cohort(InformationSet I, int action, std::unordered_set<std::string> &cohort);
+
+void valid_histories_play(InformationSet& I_1, InformationSet& I_2, PokerTable& true_cards, History& current_history, InformationSet& end_I, std::vector<int>& played_actions, int current_action_index, PolicyVec& policy_obj_x, PolicyVec& policy_obj_o, std::vector<std::vector<int>>& valid_histories_list);
+
+void upgraded_get_histories_given_I(InformationSet& I, PolicyVec& policy_obj_x, PolicyVec& policy_obj_o, std::vector<std::vector<int>>& valid_histories_list);
+
+double get_expected_utility(InformationSet &I_1, InformationSet &I_2, PokerTable &true_cards, PolicyVec &policy_obj_x, PolicyVec &policy_obj_o, double probability, History& current_history, char initial_player);
+
+double get_expected_utility_parallel(InformationSet &I_1, InformationSet &I_2, PokerTable &true_cards, PolicyVec &policy_obj_x, PolicyVec &policy_obj_o, double probability, History& current_history, char initial_player);
+
+double get_expected_utility_wrapper(PolicyVec& policy_obj_x, PolicyVec& policy_obj_o, char game);
+
+double get_prob_h_given_policy(InformationSet& I_1, InformationSet& I_2, PokerTable& true_cards, int next_action, PolicyVec& policy_obj_x, PolicyVec& policy_obj_o, double probability, History history_obj, char initial_player, InformationSet& end_I);
+    
+double get_prob_h_given_policy_wrapper(InformationSet& I_1, InformationSet& I_2, PokerTable& true_cards, int next_action, PolicyVec& policy_obj_x, PolicyVec& policy_obj_o, History history_obj, InformationSet& curr_I_1, char initial_player);
+    
+double get_counter_factual_utility(InformationSet& I, PolicyVec& policy_obj_x, PolicyVec& policy_obj_o, std::vector<std::vector<int>>& starting_histories, std::vector<double>& prob_reaching_h_list);
+
+void get_probability_of_reaching_all_h(InformationSet& I, PolicyVec& policy_obj_x, PolicyVec& policy_obj_o, std::vector<std::vector<int>>& starting_histories, char initial_player, std::vector<double>& prob_reaching_h_list_all);
+
+double calc_util_a_given_I_and_action(InformationSet& I, int action, PolicyVec& policy_obj_x, PolicyVec& policy_obj_o, std::vector<std::vector<int>>& starting_histories, std::vector<double>& prob_reaching_h_list);
+
+void calc_cfr_policy_given_I(InformationSet& I, PolicyVec& policy_obj_x, PolicyVec& policy_obj_o, int T, std::vector<double>& regret_list);
+
+std::vector<std::vector<double> > get_prev_regrets(std::string& file_path, char player);
+
+bool get_move_flag(std::string I_hash, char player);
+
+void simulate_opponent_turn(PokerTable& true_cards, History& history, double reach_probability, InformationSet& opponent_I, PolicyVec& policy_obj,  
+                            std::vector<PokerTable>& true_cards_list, std::vector<History>& history_list, std::vector<double>& reach_probability_list,  
+                            std::vector<InformationSet>& opponent_I_list, std::vector<double>& Q_values, char br_player, int played_action);
+
+double get_max_Q_value_and_update_policy(std::vector<double>& Q_values, std::vector<int>& actions, PolicyVec& br, InformationSet& I);
+
+double compute_best_response(InformationSet& I, char br_player, std::vector<PokerTable>& true_cards_list, std::vector<History>& history_list, 
+                 std::vector<double>& reach_probability_list, std::vector<InformationSet>& opponent_I_list, PolicyVec& br, PolicyVec& policy_obj);
+    
+double compute_best_response_parallel(InformationSet& I, char br_player, std::vector<PokerTable>& true_cards_list, std::vector<History>& history_list, 
+                 std::vector<double>& reach_probability_list, std::vector<InformationSet>& opponent_I_list, PolicyVec& br, PolicyVec& policy_obj);
+
+double compute_best_response_wrapper(PolicyVec& policy_obj, PolicyVec& br, char br_player, char game);
+
+void save_map_txt(std::string output_file, std::vector<std::vector<double>>& map, std::vector<std::string>& Information_sets);
