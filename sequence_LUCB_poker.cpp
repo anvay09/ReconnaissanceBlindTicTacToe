f8292c0320b60a7b8cdf8878c3aa0276295f3ab4
anvay09 <shah.anvay@gmail.com>
2025-04-10 01:01:14 +0530

    debug

--- sequence_LUCB_poker.cpp
+++ sequence_LUCB_poker.cpp
@@ -287,7 +287,7 @@ void calc_br_sequence_LUCB(PolicyVec& opponent_policy, char br_player, std::vect
             // print sequence data
             std::cout << "Sequence data: " << std::endl;
             for (int i = 0; i < terminal_sequences.size(); i++) {
-                std::cout << "Sequence: " << terminal_sequences[i].hash << ", n: " << terminal_sequences[i].n << ", n_pi" << terminal_sequences[i].n_pi << ", r: " << terminal_sequences[i].r << ", p: " << terminal_sequences[i].p << ", ucb_r: " << terminal_sequences[i].ucb_r << ", ucb_p: " << terminal_sequences[i].ucb_p << std::endl;
+                std::cout << "Sequence: " << terminal_sequences[i].hash << ", n: " << terminal_sequences[i].n << ", n_pi: " << terminal_sequences[i].n_pi << ", r: " << terminal_sequences[i].r << ", p: " << terminal_sequences[i].p << ", ucb_r: " << terminal_sequences[i].ucb_r << ", ucb_p: " << terminal_sequences[i].ucb_p << std::endl;
             }
 
             double expected_utility = 0.0;
