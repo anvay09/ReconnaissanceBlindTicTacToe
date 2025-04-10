1d59180711c8b58c32e9c48995a4b3f6555c99a9
anvay09 <shah.anvay@gmail.com>
2025-04-10 14:11:31 +0530

    debug

--- sequence_LUCB_poker.cpp
+++ sequence_LUCB_poker.cpp
@@ -283,17 +283,6 @@ void calc_br_sequence_LUCB(PolicyVec& opponent_policy, char br_player, std::vect
 
     for (int t = 1; t <= iterations; t += 1) {
         if (t % log_frequency == 0) { 
-            // print sequence data
-            std::cout << "Sequence data: " << std::endl;
-            for (int i = 0; i < terminal_sequences.size(); i++) {
-                if (terminal_sequences[i].n == 0) {
-                    std::cout << "Sequence: " << terminal_sequences[i].hash << ", n: " << terminal_sequences[i].n << ", n_pi: " << terminal_sequences[i].n_pi << ", r: " << terminal_sequences[i].r << ", p: " << terminal_sequences[i].p << ", ucb_r: " << terminal_sequences[i].ucb_r << ", ucb_p: " << terminal_sequences[i].ucb_p << std::endl;
-                }
-                else {
-                    std::cout << "Sequence: " << terminal_sequences[i].hash << ", n: " << terminal_sequences[i].n << ", n_pi: " << terminal_sequences[i].n_pi << ", r: " << terminal_sequences[i].r / (double) terminal_sequences[i].n << ", p: " << terminal_sequences[i].p << ", ucb_r: " << terminal_sequences[i].ucb_r << ", ucb_p: " << terminal_sequences[i].ucb_p << std::endl;
-                }
-            }
-
             double expected_utility = 0.0;
             double exploitability = 0.0;
         
